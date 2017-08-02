0c519bdec9e438de445ca5acd4fce4a4115947fa
Hiro Obara <hobara1211@gmail.com>
2017-08-01 23:30:43 -0700

    move command added

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './lib/dancer.js',
+  entry: './lib/type_dance.js',
   output: {
    filename: './lib/bundle.js'
   },
